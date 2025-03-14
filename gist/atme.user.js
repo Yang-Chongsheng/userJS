8fda9ee0ef821716fbf4c712c43e7878b5986839
Charles Yang <csyg.code@outlook.com>
2025-03-14 22:50:31 +0800

    Update atme.user.js

--- gist/atme.user.js
+++ gist/atme.user.js
@@ -7,8 +7,8 @@
 // @match        https://www.mashibing.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mashibing.com
 // @grant        none
-// @downloadURL  
-// @updateURL    
+// @downloadURL  https://github.com/Yang-Chongsheng/userJS/raw/refs/heads/main/gist/atme.user.js
+// @updateURL    https://github.com/Yang-Chongsheng/userJS/raw/refs/heads/main/gist/atme.user.js
 // ==/UserScript==
 
 (function() {
